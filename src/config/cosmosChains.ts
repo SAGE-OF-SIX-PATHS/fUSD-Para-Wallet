a880fa31a49a341cd2a6729a3af0dde678b4678a
Akpamgbo Philip <nzubeakpamgbo>
2025-09-27 10:21:05 +0100

    store as backup

---
+++ src/config/cosmosChains.ts
@@ -0,0 +1,22 @@
+// src/config/cosmosChains.ts I added this file
+
+export const cosmoshub = {
+          chainId: "cosmoshub-4",
+          chainName: "Cosmos Hub",
+          rpc: "https://rpc.cosmoshub.strange.love",
+          rest: "https://api.cosmoshub.strange.love",
+};
+
+export const osmosis = {
+          chainId: "osmosis-1",
+          chainName: "Osmosis",
+          rpc: "https://rpc.osmosis.zone",
+          rest: "https://lcd.osmosis.zone",
+};
+
+export const noble = {
+          chainId: "noble-1",
+          chainName: "Noble",
+          rpc: "https://rpc.mainnet.noble.strange.love",
+          rest: "https://api.mainnet.noble.strange.love",
+};
